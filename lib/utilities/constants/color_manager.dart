94f69a0d25491affb21c7777fa56b2d642c68324
akhaled <akhaled@cairo.aas.com.sa>
2022-08-21 01:04:30 +0200

    finish onboarding screen

---
+++ lib/utilities/constants/color_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorManager {
+  static const Color kPrimaryColor = Color(0xFFc8b6ff);
+  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kGrey = Color(0xFF9E9E9E);
+}
